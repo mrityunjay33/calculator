42d2a9ab170eced842272cd05cb15974441a5e3c
Mrityunjay Kumar <mk10399@gmail.com>
2023-12-25 18:24:13 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const handleIncrement = () => {
-    setCount((prevCount) => prevCount+1);
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    if(typeof(parseInt(e.target.innerText)) === 'number')
+    setInputValue((prevValue)=> `${prevValue}${e.target.innerText}`);
   }
-  const handleDecrement = () => {
-    setCount((prevCount) => prevCount-1);
+  const evaluate = (e) => {
+    const res = eval(inputValue);
+    setResult(res);
   }
+
+  const handleClear = (e) => {
+    setInputValue('');
+    setResult(null);
+  }
+
 
   return (
     <div className='container'>
-      <h1>Calculator</h1>
-
+      <h1>React Calculator</h1>
+      <input type="text" className="form-control" placeholder="" value={inputValue} readOnly/>
+      <div className='result'>{result}</div>
+      <div className='btn-container'>
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>+</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>-</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>*</button>
+        <button onClick={handleClear}>C</button>
+        <button onClick={handleClick}>0</button>
+        <button onClick={evaluate}>=</button>
+        <button onClick={handleClick}>/</button>
+    </div>
     </div>
   );
 }
